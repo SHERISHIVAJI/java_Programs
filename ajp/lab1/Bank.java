8349fc9c38cd15832dc16299d858ebbc72c81664
SHERI SHIVAJI <shivajisheri@gmail.com>
2025-04-16 20:50:13 +0530

    Bank application for withdraw and deposit simple java

--- ajp/lab1/Bank.java
+++ ajp/lab1/Bank.java
@@ -1,18 +1,18 @@
+/*
+Create a Bank class and declare an instance variable named amount of 
+type double.Create parameterized constructor to initialize variable
+“amount” with value 10000.Create two methods withdraw(double 
+withdrawalAmount) and deposit(double depositAmount).Calculate 
+withdrawal based on some condition (using ternary operator) like If 
+amount is sufficient then “withdraw successful” message will be 
+printed on the console and amount should be updated after withdraw. 
+Later on, deposit 5000 in the account balance.At the end display 
+total balance on the console.
+*/
 package ajp.lab1;
 
 import java.util.Scanner;
 
-/*
- *  Create a Bank class and declare an instance variable named amount of 
- *  type double.Create parameterized constructor to initialize variable
- *  “amount” with value 10000.Create two methods withdraw(double 
- *   withdrawalAmount) and deposit(double depositAmount).Calculate 
- *   withdrawal based on some condition (using ternary operator) like If 
- *   amount is sufficient then “withdraw successful” message will be 
- *   printed on the console and amount should be updated after withdraw. 
- *   Later on, deposit 5000 in the account balance.At the end display 
- *   total balance on the console.
- */
 public class Bank
 {
 	double amount;//instance variable
